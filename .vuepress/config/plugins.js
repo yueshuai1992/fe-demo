e944b2aad51149c57b53ecca1afaea1d145357b3
岳帅 <yueshuai@changba.com>
2020-12-04 14:07:31 +0800

    提交

--- .vuepress/config/plugins.js
+++ .vuepress/config/plugins.js
@@ -3,7 +3,7 @@ module.exports = [
   [require('../plugins/run'), {
     jsLabs: [
       'https://cdn.bootcdn.net/ajax/libs/vue/2.6.9/vue.js',
-      'https://cdn.jsdelivr.net/npm/vue-luck-draw/dist/vue-luck-draw.umd.min.js',
+      'https://cdn.jsdelivr.net/npm/vue-luck-draw/dist/vue-luck-draw.umd.min.js'
     ]
   }],
 ]
